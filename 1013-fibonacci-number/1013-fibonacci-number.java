dcfe1de8474ebabcb93b7896bbbf280963968853
harshils20 <108141408+harshils20@users.noreply.github.com>
2025-05-12 12:57:35 +0530

    Time: 8 ms (43.4%), Space: 40.8 MB (18.56%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if (n <=1) {
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+        
+    }
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Solution obj  = new Solution();
+        System.out.println("fibonacci series upto "+ n +"terms:");
+
+        for(int i=0;i<n;i++) {
+            System.out.print(obj.fib(i) + " ");
+        }
+    }
+}
